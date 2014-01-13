efa646e173dd02a03f1f05306a242aef9cba0ef0
Felipe Norato Lacerda <fnoratol@gmail.com>
2014-01-13 11:23:44 -0200

    Add the stylesheet file

--- src/ClassDiagram.js
+++ src/ClassDiagram.js
@@ -10,22 +10,16 @@ ClassDiagram.prototype.draw_class = function(class_name) {
 
 function draw_div(class_name) {
   div = $("#Diagram_Getter");
-  div.append("<div id='"+class_name+"'></div>");
-  class_div = $("#"+class_name+"");
-  class_div.css({'width':'150px', 'height':'200px','border':'2px solid black'});
+  div.append("<div class='class_element' id='"+class_name+"'></div>");
 };
 
 
 function set_class_name(class_name) {
   class_div = $("#"+class_name+"");
   class_div.append("<div id='title'>"+class_name+"</div>");
-  title = $("#title");
-  title.css({'text-align':'center'})
 };
 
 function insert_attrs (class_name) {
   class_div = $("#"+class_name+"");
-  class_div.append("<div id='"+class_name+"attrs'></div>");
-  attr_div = $("#"+class_name+"attrs");
-  attr_div.css({'width':'150px', 'height':'50px','border':'2px solid black'});
-}
+  class_div.append("<div class='class_attrs' id='"+class_name+"attrs'></div>");
+};
